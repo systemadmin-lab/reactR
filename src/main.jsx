4b7d85d003bf4f923555764297ad422dd18363e8
systemadmin-lab <rejuananik@gmail.com>
2025-07-16 02:13:24 +0600

    cjs

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const router =createBrowserRouter([
+  {
+    path: '/',
+    element: <div>joy bangla</div>
+  },
+  {
+    path:'/about',
+    element: <>churani chruani kali nagin jese </>
+  }
+]
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
