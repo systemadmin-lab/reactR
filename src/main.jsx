8a5d5e57e92ab57db63ee7505524e058dae40d19
systemadmin-lab <rejuananik@gmail.com>
2025-07-16 03:11:37 +0600

    cjs

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import Contact from "./Contact.jsx";
 import ErrorPage from "./Error.jsx";
 import "./index.css";
 import Root from "./Root.jsx";
+import { getContactLoader } from "./loaders/contactsLoader.js";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader:getContactLoader,
     children: [
       {
         path: "/contacts/:contacId",
