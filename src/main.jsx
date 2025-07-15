4e3b5730159deda8f3d20c11c15c5625e94bb274
systemadmin-lab <rejuananik@gmail.com>
2025-07-16 02:17:19 +0600

    cjs

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './Root.jsx'
 const router =createBrowserRouter([
   {
     path: '/',
-    element: <div>joy bangla</div>
+    element: <Root/>,
   },
   {
     path:'/about',
